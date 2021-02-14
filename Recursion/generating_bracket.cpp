8a65613ec9ca80134aee8832c30aff170f91e4f5
mukul <taraniamukul@gmail.com>
2021-02-14 22:19:56 +0530

    Recursion and BackTracking

---
+++ Recursion/generating_bracket.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+//Generate Balanced Brackets using N pairs of Round Brackets
+void generate_brackets(char *out, int n, int idx, int open, int close){
+    //base case
+    if(idx == 2*n){
+        out[idx] = '\0';
+        cout<<out<<endl;
+        return;
+    }
+
+    //Rec case
+    //2 options
+    if(open < n){
+        out[idx] = '(';
+        generate_brackets(out, n, idx+1, open+1, close);
+    }
+
+    if(close < open){
+        out[idx] = ')';
+        generate_brackets(out, n, idx + 1, open, close+1);
+    }
+
+    return;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    char out[100];
+    int idx = 0;
+    generate_brackets(out, n, idx, 0, 0);
+};
